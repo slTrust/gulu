7eacdb9ca57393c970b42ff91e4b6ba993b35dca
huangjx <trustfor@sina.cn>
2018-06-24 18:30:06 +0800

    完成按钮基本样式

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # 轱辘 - 一个Vue UI 组件
 
 作者：hjx
+
+
+## 安装
+
+使用本框架前，请在CSS中开启 border-box
+
+```
+*{box-sizing: border-box;}
+```
---
+++ button.js
@@ -0,0 +1,8 @@
+/**
+ * Created by hjx on 2018/6/24.
+ */
+Vue.component('g-button',{
+    template:`
+        <button class="g-button">按钮</button>
+    `
+});
